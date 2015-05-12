655bffa1c32b10c8b4fa9a46dc63287ca0f36b83
Gunnar Giffey <gunnar@textcontrol.com>
2015-05-12 14:38:27 +0200

    Added content.

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApplication11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonBar1 = new TXTextControl.ButtonBar();
+            this.rulerBar1 = new TXTextControl.RulerBar();
+            this.statusBar1 = new TXTextControl.StatusBar();
+            this.mailMerge1 = new TXTextControl.DocumentServer.MailMerge(this.components);
+            this.textControl1 = new TXTextControl.TextControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mergeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mergeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonBar1
+            // 
+            this.buttonBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonBar1.Location = new System.Drawing.Point(0, 24);
+            this.buttonBar1.Name = "buttonBar1";
+            this.buttonBar1.Size = new System.Drawing.Size(1078, 28);
+            this.buttonBar1.TabIndex = 1;
+            this.buttonBar1.Text = "buttonBar1";
+            // 
+            // rulerBar1
+            // 
+            this.rulerBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rulerBar1.Location = new System.Drawing.Point(0, 52);
+            this.rulerBar1.Name = "rulerBar1";
+            this.rulerBar1.Size = new System.Drawing.Size(1078, 25);
+            this.rulerBar1.TabIndex = 3;
+            this.rulerBar1.Text = "rulerBar1";
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusBar1.Location = new System.Drawing.Point(0, 429);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(1078, 22);
+            this.statusBar1.TabIndex = 2;
+            // 
+            // mailMerge1
+            // 
+            this.mailMerge1.TemplateFile = "template_removerow.docx";
+            this.mailMerge1.TextComponent = this.textControl1;
+            this.mailMerge1.BlockRowMerged += new TXTextControl.DocumentServer.MailMerge.BlockRowMergedHandler(this.mailMerge1_BlockRowMerged);
+            // 
+            // textControl1
+            // 
+            this.textControl1.ButtonBar = this.buttonBar1;
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textControl1.Location = new System.Drawing.Point(0, 77);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.RulerBar = this.rulerBar1;
+            this.textControl1.Size = new System.Drawing.Size(1078, 352);
+            this.textControl1.StatusBar = this.statusBar1;
+            this.textControl1.TabIndex = 5;
+            this.textControl1.Text = "textControl1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mergeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1078, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mergeToolStripMenuItem
+            // 
+            this.mergeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mergeToolStripMenuItem1});
+            this.mergeToolStripMenuItem.Name = "mergeToolStripMenuItem";
+            this.mergeToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.mergeToolStripMenuItem.Text = "Merge";
+            // 
+            // mergeToolStripMenuItem1
+            // 
+            this.mergeToolStripMenuItem1.Name = "mergeToolStripMenuItem1";
+            this.mergeToolStripMenuItem1.Size = new System.Drawing.Size(108, 22);
+            this.mergeToolStripMenuItem1.Text = "Merge";
+            this.mergeToolStripMenuItem1.Click += new System.EventHandler(this.mergeToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 451);
+            this.Controls.Add(this.textControl1);
+            this.Controls.Add(this.rulerBar1);
+            this.Controls.Add(this.buttonBar1);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Text Control Reporting: Removing empty table rows";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TXTextControl.ButtonBar buttonBar1;
+        private TXTextControl.RulerBar rulerBar1;
+        private TXTextControl.StatusBar statusBar1;
+        private TXTextControl.DocumentServer.MailMerge mailMerge1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mergeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mergeToolStripMenuItem1;
+        private TXTextControl.TextControl textControl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TXTextControl;
+
+namespace WindowsFormsApplication11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mergeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml("sample_db.xml", XmlReadMode.Auto);
+
+            mailMerge1.Merge(ds.Tables["Sales_SalesOrderHeader"]);
+
+            textControl1.Tables.GridLines = false;
+        }
+
+        private void mailMerge1_BlockRowMerged(object sender, TXTextControl.DocumentServer.MailMerge.BlockRowMergedEventArgs e)
+        {
+            byte[] data;
+
+            // create a temporary ServerTextControl to access API
+            using (ServerTextControl tx = new ServerTextControl())
+            {
+                tx.Create();
+                tx.Load(e.MergedBlockRow, BinaryStreamType.InternalUnicodeFormat);
+                
+                if (tx.Tables.GetItem() == null)
+                    return;
+
+                Table table = tx.Tables.GetItem();
+
+                // loop through row and remove empty rows
+                foreach (TableRow row in table.Rows)
+                {
+                    row.Select();
+                    if (tx.Selection.Length == table.Columns.Count)
+                    {
+                        tx.Selection.Length = 0;
+                        table.Rows.Remove();
+                    }
+                }
+
+                tx.Save(out data, BinaryStreamType.InternalUnicodeFormat);
+            }
+            
+            // return the manipulated merged block
+            e.MergedBlockRow = data;
+        }
+    }
+}
